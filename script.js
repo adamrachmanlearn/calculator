5b98cda5df3a01e9ef63d44c8b61836291e1737f
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-22 16:49:20 +0700

    Add variables to hold number and operator

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+let firstNum = 0;
+let operator = "";
+let secondNum = 0;
