eb0af9342edae08da39b65d5d28c279b4c7da484
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-22 16:54:47 +0700

    Add operate function

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const divide = (num1, num2) => num1 / num2;
 let firstNum = 0;
 let operator = "";
 let secondNum = 0;
+
+const operate = (num1, op, num2) => {
+    switch(op) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        default:
+            return divide(num1, num2);
+    }
+}
