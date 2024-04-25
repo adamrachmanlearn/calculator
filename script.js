2d90b621863f0b7f883a831613b67a3a4200383e
Adam <adamrachman.learn@gmail.com>
2024-04-25 14:27:26 +0700

    Make the calculator work

--- script.js
+++ script.js
@@ -4,36 +4,75 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 const button = document.querySelector(".inputArea");
 const display = document.querySelector(".displayArea");
-let displayString = "";
-
-let firstNum = 0;
-let operator = "";
-let secondNum = 0;
+const isOp = /[^0-9]/;
+let num1 = "";
+let num2 = "";
+let op = "";
 
 const operate = (num1, op, num2) => {
     switch(op) {
         case "+":
-            return add(num1, num2);
+            return add(+num1, +num2);
             break;
         case "-":
-            return subtract(num1, num2);
+            return subtract(+num1, +num2);
             break;
         case "*":
-            return multiply(num1, num2);
+            return multiply(+num1, +num2);
             break;
         default:
-            return divide(num1, num2);
+            return divide(+num1, +num2);
     }
 }
 
 button.addEventListener("click", element => {
     if (element.target.className !== "inputArea") {
-        if (element.target.innerText === "Clear") {
-            displayString = "";
-            display.innerText = displayString;
+        const btn = element.target.innerText;
+        if (btn === "Clear") {
+            num1 = "";
+            num2 = "";
+            op = "";
+            display.innerText = num1;
+        } else if (btn === "=") {
+            if (num2 !== "") {
+                if (op === "/" && num2 === "0") {
+                    display.innerText = "jdon"
+                    num1 = "";
+                    num2 = "";
+                    op = "";
+                } else {
+                    num1 = operate(num1, op, num2);
+                    num2 = "";
+                    op = "";
+                    display.innerText = num1;
+                }
+            }
         } else {
-            displayString += element.target.innerText;
-            display.innerText = displayString;
+            if (isOp.test(btn)) {
+                if (num2 !== "") {
+                    if (op === "/" && num2 === "0") {
+                        display.innerText = "jdon"
+                        num1 = "";
+                        num2 = "";
+                        op = "";
+                    } else {
+                        num1 = operate(num1, op, num2);
+                        num2 = "";
+                        display.innerText = num1;
+                    }
+                }
+                op = btn;
+            } else {
+                if (op !== "") {
+                    num2 += btn;
+                    display.innerText = num2;
+                } else {
+                    num1 += btn;
+                    display.innerText = num1;
+                }
+            }
         }
+        //log every click
+        console.log("num1: " + num1 + "\nop: " + op + "\nnum2: " + num2);
     }
-})
+});
